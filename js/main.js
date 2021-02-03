80e20e601e0f8bf0b5c5987b5b54060263b74bcc
LorenzoAHenrique <lorenzoarrudahenrique@gmail.com>
2021-02-02 22:39:43 -0300

    Adicionado arquivo JS e escrito as primeiras linhas de script

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var nome = "Lorenzo Henrique";
+var n1 = "2";
+var n2 = "9";
+var idade = n1 * n2;
+var frase = "Lorem ipsum dolor sit amet"
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//alert(frase.replace("Lorem ipsum dolor sit amet","É nóis na fita menó."));
+//console.log(frase.toUpperCase());
+console.log(frase.replace("Lorem ipsum dolor sit amet", "Example"));
+console.log(nome);
+console.log(n1 * n2);
+alert(nome + " tem " + idade);
+
